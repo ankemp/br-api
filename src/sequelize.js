48004cc2767e98a7642d6d280d5b3efa83a6646a
Andrew Kemp <andrew.kemp@viacomcontractor.com>
2017-12-10 20:29:19 -0500

    CircleCI & Product environment setup

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,14 +1,39 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  const connectionString = app.get('postgres');
-  const sequelize = new Sequelize(connectionString, {
+  const defaults = {
     dialect: 'postgres',
     logging: false,
     define: {
       freezeTableName: true
     }
-  });
+  };
+  let sequelize;
+  if (process.env.NODE_ENV === 'production') {
+    const sqlUser = app.get('sqlUser');
+    const sqlPass = app.get('sqlPass');
+    const sqlDb = app.get('sqlDb');
+    const socketPath = app.get('socketPath');
+    const options = Object.assign(defaults, {
+      dialectOptions: {
+        socketPath
+      }
+    });
+    sequelize = new Sequelize(sqlDb, sqlPass, sqlPass, options);
+  } else if (process.env.NODE_ENV === 'migrate') {
+    const sqlUser = app.get('sqlUser');
+    const sqlPass = app.get('sqlPass');
+    const sqlDb = app.get('sqlDb');
+    const options = Object.assign(defaults, {
+      port: '3306',
+      host: 'localhost'
+    })
+    sequelize = new Sequelize(sqlDb, sqlPass, sqlPass, options);
+  } else {
+    const connectionString = app.get('postgres');
+    const options = Object.assign(defaults, {});
+    sequelize = new Sequelize(connectionString, options);
+  }
   const oldSetup = app.setup;
 
   app.set('sequelizeClient', sequelize);
