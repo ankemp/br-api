2b341f5387a5f4f769433c50f4cb2c941e935bcb
Andrew Kemp <andrew@ankemp.com>
2017-12-12 15:45:34 -0500

    Adding hook to pluck matches from parent

---
+++ src/hooks/pluck-matches.js
@@ -0,0 +1,11 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
+  return async context => {
+    if (!!context.result && !!context.result.data) {
+      context.result.data = context.result.data.map(parent => parent.matches)
+    }
+    return context;
+  };
+};
