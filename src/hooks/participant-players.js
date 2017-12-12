33b2e273d2538071b7e2fe24bc6f3a71c80f22ec
Andrew Kemp <andrew@ankemp.com>
2017-12-12 13:31:43 -0500

    Fixing tests

--- src/hooks/participant-players.js
+++ src/hooks/participant-players.js
@@ -3,14 +3,16 @@
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
-    const participants = context.data;
-    const playersService = context.app.service('players');
-    participants.forEach(participant => {
-      playersService.get(participant.player.id)
-        .catch(() => {
-          playersService.create(participant.player.id);
-        })
-    });
-    return context;
+    if (!!context.data && !!context.app) {
+      const participants = context.data;
+      const playersService = context.app.service('players');
+      participants.forEach(participant => {
+        playersService.get(participant.player.id)
+          .catch(() => {
+            playersService.create(participant.player.id);
+          })
+      });
+      return context;
+    }
   };
 };
--- src/hooks/roster-participants.js
+++ src/hooks/roster-participants.js
@@ -3,18 +3,20 @@
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
-    const rosters = context.data;
-    const playersService = context.app.service('players');
-    rosters.forEach(roster => {
-      const participants = roster.participants.map(participant => {
-        participant.matchId = roster.matchId;
-        participant.rosterId = roster.id;
-        participant.championId = participant.champion.id;
-        participant.playerId = participant.player.id;
-        return participant;
+    if (!!context.data && !!context.app) {
+      const rosters = context.data;
+      const playersService = context.app.service('players');
+      rosters.forEach(roster => {
+        const participants = roster.participants.map(participant => {
+          participant.matchId = roster.matchId;
+          participant.rosterId = roster.id;
+          participant.championId = participant.champion.id;
+          participant.playerId = participant.player.id;
+          return participant;
+        });
+        context.app.service('participants').create(participants);
       });
-      context.app.service('participants').create(participants);
-    });
+    }
     return context;
   };
 };
