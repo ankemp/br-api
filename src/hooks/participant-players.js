222cfaadc76296737bb3a18b57900d222e9b8533
Andrew Kemp <andrew@ankemp.com>
2017-12-14 12:19:51 -0500

    Better batch creation of players & participants

--- src/hooks/participant-players.js
+++ src/hooks/participant-players.js
@@ -1,15 +1,39 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
+const _ = require('lodash');
+const brApi = require('../battlerite-api');
+const map = require('../battlerite-api/entitymapper');
+
+function savePlayers(app, players) {
+  const playersService = app.service('players');
+  return playersService.create(players);
+}
+
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
     if (!!context.data && !!context.app) {
-      const participants = context.data;
-      const playersService = context.app.service('players');
-      return Promise.all(participants.map(participant => {
-        return playersService.get(participant.player.id)
-          .catch(() => playersService.create(participant.player))
-      }))
+      const sequelizeClient = context.app.get('sequelizeClient');
+      let participants = context.data;
+      participants = _.flatten(participants);
+      const playerIds = participants.map(({ player }) => player.id);
+
+      return Promise.all(
+        playerIds.filter(async (id) =>
+          sequelizeClient.models.players
+            .findAndCount({ where: { id: id } })
+            .then(({ count }) => count === 0)
+        )
+      )
+        .then(playerIds => _.chunk(playerIds, 6))
+        .then(playerIdChunks =>
+          Promise.all(playerIdChunks.map(async (idsChunk) => {
+            const response = await brApi.getPlayers({ playerIds: idsChunk });
+            return map.players(response);
+          }))
+        )
+        .then(playersChunked => _.flatten(playersChunked))
+        .then(players => savePlayers(context.app, players))
         .then(() => context);
     }
     return context;
--- src/hooks/roster-participants.js
+++ src/hooks/roster-participants.js
@@ -1,23 +1,26 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
+const _ = require('lodash');
+
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
     if (!!context.data && !!context.app) {
       const rosters = context.data;
       const playersService = context.app.service('players');
-      return Promise.all(rosters.map(roster => {
-        return context.app
-          .service('participants')
-          .create(roster.participants.map(participant => {
-            participant.createdAt = roster.createdAt;
-            participant.matchId = roster.matchId;
-            participant.rosterId = roster.id;
-            participant.playerId = participant.player.id || '1337_bot';
-            return participant;
-          }));
-      }))
-        .then(() => context);
+
+      let participants = rosters.map(roster => {
+        return roster.participants.map(participant => {
+          participant.createdAt = roster.createdAt;
+          participant.matchId = roster.matchId;
+          participant.rosterId = roster.id;
+          participant.playerId = participant.player.id || '1337_bot';
+          return participant;
+        });
+      });
+      participants = _.flatten(participants);
+
+      return context.app.service('participants').create(participants).then(() => context);
     }
     return context;
   };
