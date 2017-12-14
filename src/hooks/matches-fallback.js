1159314c82fce759ddaa4a8e25f82966391979da
Andrew Kemp <andrew@ankemp.com>
2017-12-14 11:18:27 -0500

    Cleaning up matches fallback hook

--- src/hooks/matches-fallback.js
+++ src/hooks/matches-fallback.js
@@ -4,6 +4,46 @@
 const brApi = require('../battlerite-api');
 const map = require('../battlerite-api/entitymapper');
 
+async function saveMatches(app, matches) {
+  const matchesService = app.service('matches');
+  const sequelizeClient = app.get('sequelizeClient');
+
+  return Promise.all(
+    matches.map(match => {
+      const { rounds, rosters } = match;
+
+      return sequelizeClient.models.matches
+        .findAndCount({ where: { id: match.id } })
+        .then(({ count }) => count === 0)
+        .then(cont => cont ? matchesService.create(match) : false)
+        .then(cont => {
+          if (cont) {
+            return app
+              .service('rounds')
+              .create(rounds.map(round => {
+                round.createdAt = match.createdAt;
+                round.matchId = match.id;
+                return round;
+              }))
+          }
+          return false;
+        })
+        .then(cont => {
+          if (cont) {
+            return app
+              .service('rosters')
+              .create(rosters.map(roster => {
+                roster.createdAt = match.createdAt;
+                roster.matchId = match.id;
+                return roster;
+              }));
+          }
+          return false;
+        });
+    })
+  )
+}
+
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
     if (!!context.app && context.method === 'find') {
@@ -11,40 +51,11 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
         let params = Object.assign({}, { fromDate: context.params.fallbackFrom });
         const response = await brApi.searchMatches(params || {});
         const matches = map.matches(response);
-        const matchesService = context.app.service('matches');
+
         context.result.data = matches;
         context.result.total = matches.length;
-        return Promise.all(matches.map(match => {
-          const { rounds, rosters } = match;
 
-          
-      var count = context.app.get('sequelizeClient').models.matches.findAndCount({
-          where: {id:match.id}
-      })
-      .then(function (result) {
-          if(result.count ==0)
-              matchesService.create(match)
-                .then(() => {
-                  return context.app
-                    .service('rounds')
-                    .create(rounds.map(round => {
-                      round.createdAt = match.createdAt;
-                      round.matchId = match.id;
-                      return round;
-                    }));
-                })
-                .then(() => {
-                  return context.app
-                    .service('rosters')
-                    .create(rosters.map(roster => {
-                      roster.createdAt = match.createdAt;
-                      roster.matchId = match.id;
-                      return roster;
-                    }));
-                });
-            });
-        }))
-          .then(() => context);
+        return saveMatches(context.app, matches).then(() => context);
       }
     }
     return context;
