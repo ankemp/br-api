22d14377815b07b4c5fe86ba7fab0623c3242506
Andrew Kemp <andrew@ankemp.com>
2017-12-17 20:00:16 -0500

    Catching if player exists

--- src/hooks/players-fallback.js
+++ src/hooks/players-fallback.js
@@ -7,14 +7,23 @@ const map = require('../battlerite-api/entitymapper');
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
     if (!!context.app && context.method === 'get') {
+      const sequelizeClient = context.app.get('sequelizeClient');
       const playersService = context.app.service('players');
       const response = await brApi.getPlayer(context.id);
       const player = map.player(response);
-      return playersService
-        .create(player)
+      return sequelizeClient.models.players
+        .findAndCount({ where: { id: player.id } })
+        .then(({ count }) => count !== 0)
+        .then(exists => {
+          if (exists) {
+            return playersService.create(player);
+          }
+          return playersService.patch(player.id, player);
+        })
         .then(player => {
           context.result = {};
           context.result.data = player;
+          return;
         })
         .then(() => context);
     }
