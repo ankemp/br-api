8b6cc0e42e8e662da25f86645c730c44bd4adf97
Andrew Kemp <andrew@ankemp.com>
2017-12-17 19:53:43 -0500

    Adding player fallback

--- src/hooks/players-fallback.js
+++ src/hooks/players-fallback.js
@@ -6,18 +6,17 @@ const map = require('../battlerite-api/entitymapper');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
-    if (!!context.app) {
-      // const playersService = context.app.service('players');
-      if (context.method === 'find') {
-        // const response = await brApi.getPlayers();
-      } else if (context.method === 'get') {
-        // const response = await brApi.getPlayer(context.id);
-        // const player = map.player(response);
-        // playersService.create(player);
-        // context.result = {};
-        // context.result.data = player;
-        // console.log(context.result);
-      }
+    if (!!context.app && context.method === 'get') {
+      const playersService = context.app.service('players');
+      const response = await brApi.getPlayer(context.id);
+      const player = map.player(response);
+      return playersService
+        .create(player)
+        .then(player => {
+          context.result = {};
+          context.result.data = player;
+        })
+        .then(() => context);
     }
     return context;
   };
