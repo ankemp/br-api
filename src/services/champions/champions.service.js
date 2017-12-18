9de7f7fb9b32e87f9141f14fd44f167bb2823ef8
Andrew Kemp <andrew@ankemp.com>
2017-12-17 19:46:01 -0500

    Removing pagination on champion service

--- src/services/champions/champions.service.js
+++ src/services/champions/champions.service.js
@@ -5,12 +5,11 @@ const hooks = require('./champions.hooks');
 
 module.exports = function (app) {
   const Model = createModel(app);
-  const paginate = app.get('paginate');
 
   const options = {
     name: 'champions',
     Model,
-    paginate
+    paginate: false
   };
 
   // Initialize our service with any options it requires
