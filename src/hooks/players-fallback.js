59d6eea01919cb59c8fb0565e38003a60f6a52ac
Andrew Kemp <andrew@ankemp.com>
2017-12-18 12:18:21 -0500

    Cleanup

--- src/hooks/players-fallback.js
+++ src/hooks/players-fallback.js
@@ -13,37 +13,18 @@ function getPlayerData(playerId) {
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return async context => {
-    if (!!context.app) {
+    if (!!context.app && _.isUndefined(context.params.fallbackFrom)) {
       const sequelizeClient = context.app.get('sequelizeClient');
       const playersService = context.app.service('players');
 
-      if (_.isUndefined(context.params.fallbackFrom)) {
-        if (context.type === 'before') {
-          return sequelizeClient.models.players
-            .findAndCount({ where: { id: context.id } })
-            .then(({ count }) => count !== 0)
-            .then(exists => {
-              if (!exists) {
-                return getPlayerData(context.id)
-                  .then(player => playersService.create(player))
-                  .then(player => {
-                    context.result = {};
-                    context.result.data = player;
-                    return;
-                  })
-                  .then(() => context);
-              }
-            });
-        }
-
-        if (context.type === 'after') {
-          const exists = !!context.result.id;
-          if (exists) {
-            const updatedAt = moment(context.result.updatedAt);
-            const now = moment().subtract(6, 'hours');
-            if (!!!context.result.name || updatedAt.isBefore(now)) {
+      if (context.type === 'before') {
+        return sequelizeClient.models.players
+          .findAndCount({ where: { id: context.id } })
+          .then(({ count }) => count !== 0)
+          .then(exists => {
+            if (!exists) {
               return getPlayerData(context.id)
-                .then(player => playersService.patch(context.id, player))
+                .then(player => playersService.create(player))
                 .then(player => {
                   context.result = {};
                   context.result.data = player;
@@ -51,6 +32,23 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
                 })
                 .then(() => context);
             }
+          });
+      }
+
+      if (context.type === 'after') {
+        const exists = !!context.result.id;
+        if (exists) {
+          const updatedAt = moment(context.result.updatedAt);
+          const now = moment().subtract(6, 'hours');
+          if (!!!context.result.name || updatedAt.isBefore(now)) {
+            return getPlayerData(context.id)
+              .then(player => playersService.patch(context.id, player))
+              .then(player => {
+                context.result = {};
+                context.result.data = player;
+                return;
+              })
+              .then(() => context);
           }
         }
       }
