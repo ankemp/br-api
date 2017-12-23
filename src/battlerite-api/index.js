d4323684618182116c91b9eb299634d62ee624c2
Andrew Kemp <andrew@ankemp.com>
2017-12-23 17:45:38 -0500

    Updating api library to handle new endpoint

--- src/battlerite-api/index.js
+++ src/battlerite-api/index.js
@@ -2,5 +2,6 @@ module.exports = {
   getMatch: require('./methods/getMatch'),
   searchMatches: require('./methods/searchMatches'),
   getPlayer: require('./methods/getPlayer'),
-  getPlayers: require('./methods/getPlayers')
+  getPlayers: require('./methods/getPlayers'),
+  getTeams: require('./methods/getTeams')
 };
---
+++ src/battlerite-api/methods/getTeams.js
@@ -0,0 +1,16 @@
+const fetch = require('../fetch');
+const { queryArray } = require('../utils');
+
+module.exports = function (filters, count = 5) {
+  let params = {};
+  if (filters.playerIds) {
+    params['filter[playerIds]'] = queryArray(filters.playerIds);
+  }
+
+  if (filters.season) {
+    params['filter[season]'] = queryArray(filters.season);
+  }
+
+  const options = { qs: params };
+  return fetch('teams', options);
+}
