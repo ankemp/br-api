7c4a683a4b7fb3addcfc57061caf66ad8a9d1d38
Andrew Kemp <andrew@ankemp.com>
2018-01-08 23:54:11 -0500

    Removing outdated tests

--- test/hooks/should-fallback.test.js
+++ test/hooks/should-fallback.test.js
@@ -15,48 +15,4 @@ describe('\'shouldFallback\' hook', () => {
     });
   });
 
-  it('falbackFrom should be false', () => {
-    const mock = {
-      params: {
-        query: {
-          fallbackFrom: undefined
-        }
-      },
-      result: {
-        data: [
-          { createdAt: new Date() }
-        ]
-      }
-    };
-    const hook = shouldFallback();
-
-    return hook(mock).then(result => {
-      mock.params.query.fallbackFrom = false;
-      assert.deepEqual(result, mock, 'Returns the expected hook object');
-    });
-  });
-
-  it('falbackFrom should be a date', () => {
-    const timeAgo = new Date();
-    timeAgo.setMinutes(timeAgo.getMinutes() - 45);
-    const mock = {
-      params: {
-        query: {
-          fallbackFrom: undefined
-        }
-      },
-      result: {
-        data: [
-          { createdAt: timeAgo }
-        ]
-      }
-    };
-    const hook = shouldFallback();
-
-    return hook(mock).then(result => {
-      mock.params.query.fallbackFrom = timeAgo;
-      assert.deepEqual(result, mock, 'Returns the expected hook object');
-    });
-  });
-
 });
